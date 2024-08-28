240d8eeef9583770bcabeea2cad9310f256828a1
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 00:54:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 
-st.title("ENCRYPTION TEACHING TOOL")
-st.write(
-  "Select the encryption method that you would like to discover more about"
-)
+st.title("ENCRYPTION TEACHING TOOL?")
+st.write("Select the encryption method that you would like to discover more about")
 
 
 st.button("Reset", type="primary")
@@ -13,11 +11,11 @@ else:
     st.write("Goodbye")
 
 
-title = st.text_input("Movie title", "Life of Brian")
-st.write("The current movie title is", title)
+# title = st.text_input("Movie title", "Life of Brian")
+# st.write("The current movie title is", title)
 
 
-st.page_link("your_app.py", label="Home", icon="🏠")
-st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
-st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
-st.page_link("http://www.google.com", label="Google", icon="🌎")
+# st.page_link("your_app.py", label="Home", icon="🏠")
+# st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
+# st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
+# st.page_link("http://www.google.com", label="Google", icon="🌎")
