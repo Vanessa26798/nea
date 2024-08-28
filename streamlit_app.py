103279fe3638eb4974ea5f9f9ed424eb00ebc0ac
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 00:57:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,3 @@
 import streamlit as st
 
 st.title("ENCRYPTION TEACHING TOOL?")
-st.write("Select the encryption method that you would like to discover more about")
-
-
-st.button("Reset", type="primary")
-if st.button("Say hello"):
-    st.write("Why hello there")
-else:
-    st.write("Goodbye")
