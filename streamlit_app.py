df6bdcef5f2d2c08afa5bcca10965cf961324775
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 00:51:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
 st.title("ENCRYPTION TEACHING TOOL")
-st.write("Select the encryption method that you would like to discover more about")
+st.write(
+  "Select the encryption method that you would like to discover more about"
+)
 
 
 title = st.text_input("Movie title", "Life of Brian")
