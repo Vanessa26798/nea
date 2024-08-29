095601ad42819430fee8d042d0c9e7789bc7b0d9
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 20:54:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,19 @@ else:
 #  ])
 # pg.run()
 
+
+# tab1, tab2, tab3, tab4 = st.tabs(["Encrypt a plaintext", "Decrypt a ciphertext", "History", "Weakness"])
+
+# with tab1:
+#     st.header("A cat")
+#     st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+# with tab2:
+#     st.header("A dog")
+#     st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+# with tab3:
+#     st.header("An owl")
+#     st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+# with tab4: 
+
+
+
