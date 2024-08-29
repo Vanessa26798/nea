942bb561ba504969e6644b18bcca754d778f60f3
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 23:10:37 +0100

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -2,28 +2,7 @@ import streamlit as st
 import random
 
 
-st.page_link("https://encryption-teaching-tool.streamlit.app/", label="Homepage")
-
 st.header("Caesar Encryption")
-
-tab1, tab2 = st.tabs(["Caesar Encryption", "Vernam Encryption"])
-
-with tab1:
-    st.header("Caesar Encryption")
-    Input = st.text_input("Input your own key (1) or a random key (2)? ", " ")
-    if Input == "1":
-        Key = st.text_input("Please enter the key: ", "")
-        st.write("The key is ", Key)
-    elif Input == "2": 
-        Key = random.randrange(1, 26)
-        st.write("The key is ", Key)
-    elif Input != 1 and Input != 2: 
-        st.write("Invalid input.")
-
-with tab2:
-    st.header("Vernam Encryption")
-    st.write("tesing")
-
 
 
 tab1, tab2, tab3, tab4 = st.tabs(["History", "Encrypt a plaintext", "Decrypt a ciphertext", "Weakness"])
