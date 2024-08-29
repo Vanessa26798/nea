68bef43f23e7b8f6bb78ba22cf893d8ee66b265b
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 01:02:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,7 @@ import streamlit as st
 st.title("ENCRYPTION TEACHING TOOL")
 st.write("Select the encryption method that you would like to discover more about")
 
-
-st.button("Reset", type="primary")
-if st.button("Say hello"):
-    st.write("Why hello there")
-else:
-    st.write("Goodbye")
+st.page_link("your_app.py", label="Home", icon="🏠")
+st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
+st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
+st.page_link("http://www.google.com", label="Google", icon="🌎")
