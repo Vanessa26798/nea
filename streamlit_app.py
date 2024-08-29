ea0bbabee47f0c4813a58f4f43d6f370d4acdaa5
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 21:46:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,25 +16,6 @@ with col1:
     st.info('Uses ...')
 if st.button("Caesar cipher"):
      st.page_link("nea/page2")
-     
-  tab1, tab2 = st.tabs(["Caesar Encryption", "Vernam Encryption"])
-
-  with tab1:
-      st.header("Caesar Encryption")
-      Input = st.text_input("Input your own key (1) or a random key (2)? ", " ")
-      if Input == "1":
-          Key = st.text_input("Please enter the key: ", "")
-          st.write("The key is ", Key)
-      elif Input == "2": 
-          Key = random.randrange(1, 26)
-          st.write("The key is ", Key)
-      elif Input != 1 and Input != 2: 
-          st.write("Invalid input.")
-
-  with tab2:
-      st.header("Vernam Encryption")
-      st.write("tesing")
-
 
 with col2:
     st.header("Vernam cipher")
