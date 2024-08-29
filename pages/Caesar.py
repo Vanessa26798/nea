391f0ab3e82b6d4e6bca43e2d6ba3e2a78efd82e
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 23:12:48 +0100

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -9,17 +9,17 @@ tab1, tab2, tab3, tab4 = st.tabs(["History", "Encrypt a plaintext", "Decrypt a c
 
 with tab1:
     st.header("History")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
 with tab2:
     st.header("Encryt a plaintext")
-    Input = st.text_input("Input your own key (1) or a random key (2)? ", " ")
+    Input = st.text_input("Input your own key (1) or a random key (2)? ", value="")
     if Input == "1":
-        Key = st.text_input("Please enter the key: ", "")
+        Key = st.text_input("Please enter the key: ", value="")
         st.write("The key is ", Key)
     elif Input == "2": 
         Key = random.randrange(1, 26)
         st.write("The key is ", Key)
-    elif Input != 1 and Input != 2: 
+    elif Input != 1 and Input != 2 and Input != "": 
         st.write("Invalid input.")
 with tab3:
     st.header("Decrypt a ciphertext")
