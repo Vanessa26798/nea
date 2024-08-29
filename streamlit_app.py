6b18932d8e5fee9fb6bce24dda1e9948cda3b1fb
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 01:24:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,23 +5,30 @@ st.title("ENCRYPTION TEACHING TOOL")
 st.write("Select the encryption method that you would like to discover more about")
 
 
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+tab1, tab2, tab3 = st.tabs(["Caesar Encryption", "Vernam Encryption"])
 
 with tab1:
     st.header("Caesar Encryption")
+    Input = st.text_input("Input your own key (1) or a random key (2)? ", "")
+    if Input == "1":
+        Key = st.text_input("Please enter the key: ", "")
+        st.write("The key is ", Key)
+    elif Input == "2": 
+        Key = random.randrange(1, 26)
+        st.write("The key is ", Key)
+    elif Input != 1 and Input != 2: 
+        st.write("Invalid input.")
 
 with tab2:
     st.header("Vernam Encryption")
+    Input = st.text_input("Input your own key (1) or a random key (2)? ", "")
+    if Input == "1":
+        Key = st.text_input("Please enter the key: ", "")
+        st.write("The key is ", Key)
+    elif Input == "2": 
+        Key = random.randrange(1, 26)
+        st.write("The key is ", Key)
+    elif Input != 1 and Input != 2: 
+        st.write("Invalid input.")
 
-
-
-Input = st.text_input("Input your own key (1) or a random key (2)? ", "")
-if Input == "1":
-  Key = st.text_input("Please enter the key: ", "")
-  st.write("The key is ", Key)
-elif Input == "2":  
-  Key = random.randrange(1, 26)
-  st.write("The key is ", Key)
-elif Input != 1 and Input != 2: 
-  st.write("Invalid input.")
 
