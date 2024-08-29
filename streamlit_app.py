da944a8de1e3e586e51bd5656ff0bab235de8e63
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 01:21:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,29 @@ st.title("ENCRYPTION TEACHING TOOL")
 st.write("Select the encryption method that you would like to discover more about")
 
 
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+with tab2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+with tab3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
+
+
+
+
 Input = st.text_input("Input your own key (1) or a random key (2)? ", "")
 if Input == "1":
   Key = st.text_input("Please enter the key: ", "")
+  st.write("The key is ", Key)
 elif Input == "2":  
   Key = random.randrange(1, 26)
   st.write("The key is ", Key)
 elif Input != 1 and Input != 2: 
   st.write("Invalid input.")
 
-
-# Key = input("Input your own key (1) or a random key (2)? ")
-# if Key == "1":
-#   Key = input("Please enter the key: ")
-#   while Key.isdigit() == False:
-#    print("Invalid key")
-#    Key = input("Please enter the key: ")
-# elif Key == "2": 
-#   Key = random.randrange(1, 26)
-#   print(Key)
-# while Key != 1 and Key != 2: 
-#   Key = input("input your own key (1) or a random key (2)")
