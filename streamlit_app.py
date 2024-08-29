d718ee5dbe38d62dfa3ace1962f0af14531293ab
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 20:50:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,11 +32,11 @@ else:
     st.write("Goodbye")
 
 
-def page2():
- st.title("Second page")
- pg = st.navigation([
-    st.Page("page1.py", title="First page", icon="🔥"),
-    st.Page(page2, title="Second page", icon=":material/favorite:"),
- ])
-pg.run()
+# def page2():
+#  st.title("Second page")
+#  pg = st.navigation([
+#     st.Page("page1.py", title="First page", icon="🔥"),
+#     st.Page(page2, title="Second page", icon=":material/favorite:"),
+#  ])
+# pg.run()
 
