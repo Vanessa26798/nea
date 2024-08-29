3c291541c9e6732dbe8e6816de70c1791ec0ff09
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 01:04:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 st.title("ENCRYPTION TEACHING TOOL")
 st.write("Select the encryption method that you would like to discover more about")
 
-st.page_link("your_app.py", label="Home", icon="🏠")
-st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
-st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
-st.page_link("http://www.google.com", label="Google", icon="🌎")
+
+title = st.text_input("Movie title", "Life of Brian")
+st.write("The current movie title is", title)
