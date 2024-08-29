fff4905086089d4d9c4c0f4f76673486596a001c
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 01:12:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ if Input == "1":
 elif Input == "2":  
   Key = random.randrange(1, 26)
   st.write("The key is ", Key)
-
+while Key != 1 and Key != 2: 
+  Input = st.text_input("Invalid choice."")
 
 
 # Key = input("Input your own key (1) or a random key (2)? ")
