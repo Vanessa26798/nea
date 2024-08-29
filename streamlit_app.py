f3daedbb6597e80f32a768616ee10cc43933d5db
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 21:07:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,15 @@ st.write("Select the encryption method that you would like to discover more abou
 
 st.button("Caesar cipher", disabled=False)
 st.info('Uses ...', icon="ℹ️")
-if st.button("Caesar cipher"):
+if st.button("Caesar cipher") == True:
     print("C")
     
 
 
 st.button("Vernam Cipher", disabled=False)
 st.info('Uses ...', icon="ℹ️")
-if st.button("Vernam Cipher"):
-    print("V")
+# if st.button("Vernam Cipher"):
+#     print("V")
 
 
 # def page2():
