2d1bc083260c252054d31c6611c497da4a55d891
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-29 01:22:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,10 @@ st.write("Select the encryption method that you would like to discover more abou
 tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
 with tab1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    st.header("Caesar Encryption")
+
 with tab2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-with tab3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
-
-
+    st.header("Vernam Encryption")
 
 
 
