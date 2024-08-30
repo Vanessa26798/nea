e32944da3ab66673febd7656cd9792b5d446730e
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-30 16:41:17 +0100

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import random
+Alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
 
 
 st.header("Caesar Encryption")
