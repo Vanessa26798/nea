4aa2d02d6c4e5eb3436b1ecdc045f43081a84407
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-30 17:43:38 +0100

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -28,7 +28,7 @@ with tab2:
         st.write("The key is ", Key)
     elif Input != 1 and Input != 2 and Input != "": 
         st.write("Invalid input.")
-    if Plaintext.isupper() == True:
+    if Plaintext.isupper() == True and Key != "":
        Ciphertext = []
        Alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
        Key = int(Key)
