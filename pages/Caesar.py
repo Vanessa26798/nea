8849c87274acea241774b8c93f600e9cda67177a
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-31 22:10:06 +0100

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -72,8 +72,8 @@ with tab2:
              Letter_index = int(Alphabet.index(x))
              Letter_index += 1
              Letter_index += Encrypt_Key
-             while Letter_index >= 26: 
-                 Letter_index -= 26
+             while Letter_index >= 25: 
+                 Letter_index -= 25
              Letter = Alphabet[Letter_index]
              Ciphertext.append(Letter)
        st.write("".join(Ciphertext))
@@ -142,8 +142,8 @@ with tab3:
              Letter_index = int(Alphabet.index(x))
              Letter_index += 1
              Letter_index -= Decrypt_Key
-             while Letter_index >= 26: 
-                 Letter_index -= 26
+             while Letter_index >= 25: 
+                 Letter_index -= 25
              Letter = Alphabet[Letter_index]
              Plaintext.append(Letter)
        st.write("".join(Plaintext))
