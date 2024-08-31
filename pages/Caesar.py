80996bea78ac0d4b965e862f649959d37c7dc17a
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-31 18:01:13 +0100

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -16,11 +16,11 @@ with tab2:
 
     Correct_Plaintext = False
     Plaintext = st.text_input("Please enter the plaintext in upper case, between 10-30 characters: ", value="")
-    # x = Plaintext in Alphabet
-    if Plaintext in Alphabet == False and Plaintext != "":
+    x = Plaintext in Alphabet
+    if x == False and Plaintext != "":
         st.error('Invalid plaintext.', icon="🚨")
         Correct_Plaintext = False
-    elif Plaintext in Alphabet == True or Plaintext == " ":
+    elif x == True or Plaintext == " ":
         Correct_Plaintext = True
 
     Correct_Range = False
