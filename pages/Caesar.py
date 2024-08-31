5d6e4f0bbe3a82470a6ee274b7bad629952d173b
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-08-31 18:11:58 +0100

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -15,59 +15,62 @@ with tab2:
     st.header("Encryt a plaintext")
 
     Correct_Plaintext = False
-    Plaintext = st.text_input("Please enter the plaintext in upper case, between 10-30 characters: ", value="")
-    if Plaintext in Alphabet:
-        st.write("yes")
-    
-    # if x == False and Plaintext != "":
-    #     st.error('Invalid plaintext.', icon="🚨")
-    #     Correct_Plaintext = False
-    # elif x == True or Plaintext == " ":
-    #     Correct_Plaintext = True
+    Plaintext = st.text_input("Please enter the plaintext in upper case, between 10-30 characters: ", value="")    
+    for x in Plaintext: 
+        if Plaintext in Alphabet:
+            Plaintext_in_Alphabet = True
+        else:
+            Plaintext_in_Alphabet = False
+        
+        if Plaintext_in_Alphabet == False and Plaintext != "":
+            st.error('Invalid plaintext.', icon="🚨")
+            Correct_Plaintext = False
+        elif Plaintext_in_Alphabet == True or Plaintext == " ":
+            Correct_Plaintext = True
 
-    # Correct_Length = False
-    # if Correct_Plaintext == True:
-    #     if (len(Plaintext) < 10 or len(Plaintext) > 30) and Plaintext != "": 
-    #         st.error('Plaintext out of range.', icon="🚨")
-    #         Correct_Length = False
-    #     elif len(Plaintext) > 10 and len(Plaintext) < 30 and len(Plaintext) != 0 and Plaintext != "":
-    #         Correct_Length = True
+    Correct_Length = False
+    if Correct_Plaintext == True:
+        if (len(Plaintext) < 10 or len(Plaintext) > 30) and Plaintext != "": 
+            st.error('Plaintext out of range.', icon="🚨")
+            Correct_Length = False
+        elif len(Plaintext) > 10 and len(Plaintext) < 30 and len(Plaintext) != 0 and Plaintext != "":
+            Correct_Length = True
 
-    # Correct_Key = False
-    # Choice = st.text_input("Input your own key (1) or a random key (2)? ", value="")
-    # if Choice == "1":
-    #     Key = st.text_input("Please enter the key: ", value="")
-    #     if Key.isdigit() == True:
-    #         st.write("The key is ", Key)
-    #         Key = int(Key)
-    #         Correct_Key = True
-    #     elif Key.isdigit() == False and Key != "": 
-    #         st.error('Invalid input.', icon="🚨")
-    #         Correct_Key = True
-    # elif Choice == "2": 
-    #     Key = random.randrange(1, 26)
-    #     st.write("The key is ", Key)
-    #     Key = int(Key)
-    #     Correct_Key = True
-    # elif Choice != 1 and Choice != 2 and Choice != "": 
-    #     st.error('Invalid input.', icon="🚨")
-    #     Correct_Key = False
+    Correct_Key = False
+    Choice = st.text_input("Input your own key (1) or a random key (2)? ", value="")
+    if Choice == "1":
+        Key = st.text_input("Please enter the key: ", value="")
+        if Key.isdigit() == True:
+            st.write("The key is ", Key)
+            Key = int(Key)
+            Correct_Key = True
+        elif Key.isdigit() == False and Key != "": 
+            st.error('Invalid input.', icon="🚨")
+            Correct_Key = True
+    elif Choice == "2": 
+        Key = random.randrange(1, 26)
+        st.write("The key is ", Key)
+        Key = int(Key)
+        Correct_Key = True
+    elif Choice != 1 and Choice != 2 and Choice != "": 
+        st.error('Invalid input.', icon="🚨")
+        Correct_Key = False
 
-    # if Correct_Plaintext == True and Correct_Key == True and Correct_Length == True:
-    #    Ciphertext = ["The ciphertext is "]
+    if Correct_Plaintext == True and Correct_Key == True and Correct_Length == True:
+       Ciphertext = ["The ciphertext is "]
 
-    #    for x in Plaintext:
-    #      if x == " ":
-    #          Ciphertext.append(Letter)
-    #      else: 
-    #          Letter_index = int(Alphabet.index(x))
-    #          Letter_index += 1
-    #          Letter_index += Key
-    #          if Letter_index >= 26: 
-    #              Letter_index = Letter_index - 26
-    #          Letter = Alphabet[Letter_index]
-    #          Ciphertext.append(Letter)
-    #    st.write("".join(Ciphertext))
+       for x in Plaintext:
+         if x == " ":
+             Ciphertext.append(Letter)
+         else: 
+             Letter_index = int(Alphabet.index(x))
+             Letter_index += 1
+             Letter_index += Key
+             if Letter_index >= 26: 
+                 Letter_index = Letter_index - 26
+             Letter = Alphabet[Letter_index]
+             Ciphertext.append(Letter)
+       st.write("".join(Ciphertext))
 
 
 with tab3:
