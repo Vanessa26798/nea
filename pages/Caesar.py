8937bfca9461f1d14fa00ee95f998d6a5c5857db
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-09-05 10:48:49 +0100

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -150,27 +150,27 @@ with tab3:
 with tab4: 
     st.header("Level of security")
 
-    import pandas as pd
-    import streamlit_pandas as sp
+    # import pandas as pd
+    # import streamlit_pandas as sp
 
-    @st.cache_data
-    def load_data():
-        df = pd.read_csv(file)
-        return df
+    # @st.cache_data
+    # def load_data():
+    #     df = pd.read_csv(file)
+    #     return df
 
-    file = "../data/titanic.csv"
-    df = load_data()
-    create_data = {"Name": "text",
-                    "Sex": "multiselect",
-                    "Embarked": "multiselect",
-                    "Ticket": "text",
-                    "Pclass": "multiselect"}
+    # file = "../data/titanic.csv"
+    # df = load_data()
+    # create_data = {"Name": "text",
+    #                 "Sex": "multiselect",
+    #                 "Embarked": "multiselect",
+    #                 "Ticket": "text",
+    #                 "Pclass": "multiselect"}
 
-    all_widgets = sp.create_widgets(df, create_data, ignore_columns=["PassengerId"])
-    res = sp.filter_df(df, all_widgets)
-    st.title("Streamlit AutoPandas")
-    st.header("Original DataFrame")
-    st.write(df)
+    # all_widgets = sp.create_widgets(df, create_data, ignore_columns=["PassengerId"])
+    # res = sp.filter_df(df, all_widgets)
+    # st.title("Streamlit AutoPandas")
+    # st.header("Original DataFrame")
+    # st.write(df)
 
     st.header("Result DataFrame")
     st.write(res)
