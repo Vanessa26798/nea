c035ac7b74526518606e5d95b2f62cd890533af0
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-09-05 10:00:40 +0100

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -156,12 +156,9 @@ with tab4:
 
     # chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
-    st.bar_chart(chart_data)
-    df = pd.DataFrame(
-    np.random.randn(10, 5), columns=("col %d" % i for i in range(5))
-    )
-
-    st.table(df)
+    
+    df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+    st.dataframe(df)
 
 
     # Occurence = {}
