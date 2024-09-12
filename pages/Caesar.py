c72ddfd65171c8847270db44020e16bf684f1e7f
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-09-12 08:58:30 +0100

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -151,13 +151,13 @@ with tab4:
     
     
    Plaintext = "ABCDE"
-    Occurence = []
-    for x in Plaintext:
+   Occurence = []
+   for x in Plaintext:
       Total_occurence = len(Plaintext)
       Count = Plaintext.count(x)
       Character_occurence = round(Count/ Total_occurence * 100)
       Occurence[x] = Character_occurence, '%'
-    st.write(Occurence)
+   st.write(Occurence)
 
     
 
