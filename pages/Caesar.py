668aff595e694d57324e7b96aa9eb5b24f281948
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-09-12 09:41:19 +0100

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -159,8 +159,8 @@ with tab4:
       Count = Plaintext.count(x)
       Character_occurance = round(Count/ Total_occurance * 100)
       Occurance.append(Character_occurance)
-      df2 = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
-      my_table.add_rows(df2)
+    df2 = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+    my_table.add_rows(df2)
 
 
     chart_data = pd.DataFrame({"Letter": col0, "Occurance": col1})
