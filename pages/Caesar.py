7d515d4d88356a348576b67ef8ef9f5eb4236b6f
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-09-26 09:32:23 +0100

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -170,7 +170,7 @@ with tab4:
                   Highest_Occurance_Character = Character[x]
                   Highest_Occurance = Occurance[x]
     
-    chart.data = pd.DataFrame({"Character": Character, "Occurance": Occurance})
+    chart_data = pd.DataFrame({"Character": Character, "Occurance": Occurance})
     st.bar_chart(chart_data, x = "Character", y = "Occurance", horizontal=True)
     # st.write("The character with the highest occurance is " + Highest_Occurance_Character)
 
