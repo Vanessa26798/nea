be189c9f3b72fc83319001ad13e026d08bab6b71
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-10-17 09:31:41 +0100

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -28,20 +28,20 @@ with tab2:
     
     class Encrypt:
         def __init__(self):
-        self.send = ""
-        self.res = []
+            self.send = ""
+            self.res = []
 
     # Sender encrypts the data
         def sender(self):
-        self.send = st.text_input("Please enter the plaintext in upper case, within 10-30 characters: ", value="")
-        self.res = [ord(i) + 2 for i in self.send]  
-        print("Encrypted data:", "".join(chr(i) for i in self.res))
+            self.send = st.text_input("Please enter the plaintext in upper case, within 10-30 characters: ", value="")
+            self.res = [ord(i) + 2 for i in self.send]  
+            print("Encrypted data:", "".join(chr(i) for i in self.res))
 
     class Decrypt(Encrypt):
     # Receiver decrypts the data
         def receiver(self):
-        decrypted_data = "".join(chr(i - 2) for i in self.res)  
-        print("Decrypted data:", decrypted_data)
+            decrypted_data = "".join(chr(i - 2) for i in self.res)  
+            print("Decrypted data:", decrypted_data)
 
 # Usage
     obj = Decrypt()
