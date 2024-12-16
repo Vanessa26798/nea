29e13d3481925e7bf8f43aea8b988c930ad771c1
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-12-16 13:29:15 +0000

    Update Vernam.py

--- pages/Vernam.py
+++ pages/Vernam.py
@@ -1,9 +1,11 @@
 
+import string
 
 import streamlit as st
 import random
 
-Alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+Alphabet = string.ascii_uppercase
 
 st.header("Vernam cipher")
 
