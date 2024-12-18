50a413d0f6e98c42892306719926fa6c2f0360b0
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-12-18 12:24:02 +0000

    Update Vernam.py

--- pages/Vernam.py
+++ pages/Vernam.py
@@ -91,6 +91,8 @@ with tab2:
      for x in Plaintext_Binary:
          # XOR = [x]
          # XOR.append(Key_Binary[x])
+         st.write(x)
+         st.write(Key_Binary[x])
          result = x ^ Key_Binary[x]
          st.write(result)
      #     Ciphertext_Binary.append(result)
