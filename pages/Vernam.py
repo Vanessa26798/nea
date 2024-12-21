c0a57fe7c6627659a1278da3400baff5f40e8cec
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-12-21 16:13:38 +0000

    Update Vernam.py

--- pages/Vernam.py
+++ pages/Vernam.py
@@ -126,11 +126,11 @@ with tab2:
                         return key
                     else: 
                         return "key doesn't exist"
-            BauToText = get_key(x)
-            Ciphertext_Baudot.remove(Ciphertext_Baudot[Index])
-            Index = Index + 1
-            st.write("BauToText", BauToText)
-            Ciphertext.append(BauToText)
+                BauToText = get_key(x)
+                Ciphertext_Baudot.remove(Ciphertext_Baudot[Index])
+                Index = Index + 1
+                st.write("BauToText", BauToText)
+                Ciphertext.append(BauToText)
         st.write("The ciphertext is ", "".join(Ciphertext))              
 
 
