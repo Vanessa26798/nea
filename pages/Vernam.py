33939e7e1f08f88007a396997d4a3c674ce8df72
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2024-12-23 20:35:03 +0000

    Update Vernam.py

--- pages/Vernam.py
+++ pages/Vernam.py
@@ -144,15 +144,10 @@ with tab2:
                 Result = str(Result)
                 st.write("Result", Result)
                 Ciphertext_Letter = get_key(Result) 
-                if Ciphertext != "key doesn't exist" and " ":
-                    Ciphertext.append(Ciphertext_Letter)
-                    Key_Index = Key_Index + 1
-                    st.write("4", Key_Index)                   
-                    break 
-            # else:
-            #     Ciphertext.append(Ciphertext_Letter)
-            #     Key_Index = Key_Index + 1
-            #     st.write("4", Key_Index)    
+            elif Ciphertext != "key doesn't exist" and " ": 
+                Ciphertext.append(Ciphertext_Letter)
+                Key_Index = Key_Index + 1
+                st.write("4", Key_Index)    
         st.write(Ciphertext) 
                 
     st.write("The key is ", "".join(Key))
