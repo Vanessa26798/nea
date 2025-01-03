119b7ebf86cc56d40eb26e4bf843f85fb16edf8b
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-03 21:58:40 +0000

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -14,10 +14,10 @@ def Original_Alphabet():
         yield x + "  |  "
         time.sleep(0.04)
 
-    # Alphabet_List = []
-    # for x in Alphabet:
-    #     Alphabet_List.append(x)
-    # st.write(Alphabet_List)
+    Alphabet_List = []
+    for x in Alphabet:
+        Alphabet_List.append(x)
+    st.write(Alphabet_List)
     yield pd.DataFrame(np.random.randn(5, 10), columns = Alphabet)
 
 # df = pd.DataFrame(
