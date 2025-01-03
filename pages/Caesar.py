33d47299b4861814f3ca6c8254f99da08733d5f4
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-03 17:23:59 +0000

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -8,7 +8,7 @@ import string
 Alphabet = string.ascii_uppercase
 st.header("Caesar cipher")
 
-st.write("(21)")
+st.write("➊➋➌➍➎➏➐➑➒➓⓫⓬⓭⓮⓯⓰⓱⓲⓳⓴㉑㉒㉓㉔㉕")
 
 def stream_data():
     Spaced_Alphabet = ' '.join(Alphabet)
