0bb0cd3b0f7f8a8eef6981b2609534e8612315a0
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-08 09:04:43 +0000

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -22,7 +22,7 @@ def Original_Alphabet():
     myList = Alphabet_List.split(" ")
     st.write(myList)
     yield pd.DataFrame(
-        np.random.randn(1, 25),
+        np.random.randn(1, 2),
         columns=["a", "b"]
     )
 
