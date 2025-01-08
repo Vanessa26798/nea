31782951bdf3ca8bb22032d71c340f0aa99f5a52
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-08 09:50:49 +0000

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -83,14 +83,29 @@ with tab2:
         elif Encrypt_Choice != 1 and Encrypt_Choice != 2 and Encrypt_Choice != "": 
             st.error('Invalid input.', icon="🚨")
             Correct_Encrypt_Key = False
+
+
+    df1 = pd.DataFrame(
+        np.random.randn(50, 20), columns=("col %d" % i for i in range(20))
+    )
     
-    Alphabet_List = ' '.join(Alphabet)
-    Alphabet_List = Alphabet_List.split(" ")
-    Original_Alphabet = pd.DataFrame(columns = Alphabet_List, rows[1] == Alphabet_List)
-    Table = st.table(Original_Alphabet)
-    # Encrypted_Alphabet_List = Alphabet_List
-    # Encrypted_Alphabet = pd.DataFrame(columns = Encrypted_Alphabet_List)
-    Table.add_rows(Original_Alphabet)
+    my_table = st.table(df1)
+    
+    df2 = pd.DataFrame(
+        np.random.randn(50, 20), columns=("col %d" % i for i in range(20))
+    )
+    
+    my_table.add_rows(df2)
+
+
+    
+    # Alphabet_List = ' '.join(Alphabet)
+    # Alphabet_List = Alphabet_List.split(" ")
+    # Original_Alphabet = pd.DataFrame(columns = Alphabet_List, rows[1] == Alphabet_List)
+    # Table = st.table(Original_Alphabet)
+    # # Encrypted_Alphabet_List = Alphabet_List
+    # # Encrypted_Alphabet = pd.DataFrame(columns = Encrypted_Alphabet_List)
+    # Table.add_rows(Original_Alphabet)
 
     
     def Encrypted_Alphabet():
