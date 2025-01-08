ba63a9e65e96b8e6fb03141d4fa17caad11c9f63
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-08 09:43:44 +0000

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -88,8 +88,8 @@ with tab2:
     Alphabet_List = Alphabet_List.split(" ")
     Original_Alphabet = pd.DataFrame(columns = Alphabet_List)
     Table = st.table(Original_Alphabet)
-    Encrypted_Alphabet = Alphabet_List
-    Encrypted_Alphabet = pd.DataFrame(columns = Alphabet_List)
+    Encrypted_Alphabet_List = Alphabet_List
+    Encrypted_Alphabet = pd.DataFrame(columns = Encrypted_Alphabet_List)
     Table.add_rows(Encrypted_Alphabet)
 
     
