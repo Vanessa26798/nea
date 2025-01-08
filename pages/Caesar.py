d4520df4bc3db21f783395465d76d8986cddadd4
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-08 14:40:46 +0000

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -203,7 +203,7 @@ with tab3:
 with tab4: 
     st.header("Level of security")
 
-    Plaintext = "ABC"
+    Plaintext = "APPLES"
     st.write("The plaintext is " + Plaintext)
     Character = []
     Occurance = []
