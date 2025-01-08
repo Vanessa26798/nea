9518aed8ae56c0c85cb489931c345a1e9cc1cbb7
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-08 09:52:46 +0000

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -99,13 +99,13 @@ with tab2:
 
 
     
-    # Alphabet_List = ' '.join(Alphabet)
-    # Alphabet_List = Alphabet_List.split(" ")
-    # Original_Alphabet = pd.DataFrame(columns = Alphabet_List, rows[1] == Alphabet_List)
-    # Table = st.table(Original_Alphabet)
-    # # Encrypted_Alphabet_List = Alphabet_List
-    # # Encrypted_Alphabet = pd.DataFrame(columns = Encrypted_Alphabet_List)
-    # Table.add_rows(Original_Alphabet)
+    Alphabet_List = ' '.join(Alphabet)
+    Alphabet_List = Alphabet_List.split(" ")
+    Original_Alphabet = pd.DataFrame(np.random.radn(0, 26), columns = Alphabet_List)
+    Table = st.table(Original_Alphabet)
+    # Encrypted_Alphabet_List = Alphabet_List
+    # Encrypted_Alphabet = pd.DataFrame(columns = Encrypted_Alphabet_List)
+    Table.add_rows(Original_Alphabet)
 
     
     def Encrypted_Alphabet():
