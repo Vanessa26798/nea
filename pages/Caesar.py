9ae3f41fd027369d54fa9ac4aa0ee3c406d0cfd6
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-08 09:53:35 +0000

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -85,17 +85,17 @@ with tab2:
             Correct_Encrypt_Key = False
 
 
-    df1 = pd.DataFrame(
-        np.random.randn(50, 20), columns=("col %d" % i for i in range(20))
-    )
+    # df1 = pd.DataFrame(
+    #     np.random.randn(50, 20), columns=("col %d" % i for i in range(20))
+    # )
     
-    my_table = st.table(df1)
+    # my_table = st.table(df1)
     
-    df2 = pd.DataFrame(
-        np.random.randn(50, 20), columns=("col %d" % i for i in range(20))
-    )
+    # df2 = pd.DataFrame(
+    #     np.random.randn(50, 20), columns=("col %d" % i for i in range(20))
+    # )
     
-    my_table.add_rows(df2)
+    # my_table.add_rows(df2)
 
 
     
