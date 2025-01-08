0196f916415759009ff467260d350351835c8e17
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-08 10:16:31 +0000

    Update Caesar.py

--- pages/Caesar.py
+++ pages/Caesar.py
@@ -94,10 +94,10 @@ with tab2:
     if Correct_Encrypt_Key == True:
         One = Alphabet[Encrypt_Key:26]
         Two = Alphabet[0:Encrypt_Key]
-        Encrypted_Alphabet = One + Two
-        st.write(Encrypted_Alphabet)
-    # Encrypted_Alphabet = pd.DataFrame(columns = Encrypted_Alphabet_List)
-    # Table.add_rows(Original_Alphabet)
+        Encrypted_Alphabet_List = One + Two
+        st.write(Encrypted_Alphabet_List)
+        Encrypted_Alphabet = pd.DataFrame(columns = Encrypted_Alphabet_List)
+        Table.add_rows(Encrypted_Alphabet)
             # Spaces = ""
             # while Spaces.count("⇨") != Encrypt_Key:
             #     Spaces = "⇨-" + Spaces
