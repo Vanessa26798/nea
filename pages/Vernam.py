6913d1292bde947a78a86e524c3ca8a89cc0eab4
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-09 10:21:33 +0000

    Update Vernam.py

--- pages/Vernam.py
+++ pages/Vernam.py
@@ -137,6 +137,12 @@ with tab2:
                     Ciphertext.append(Ciphertext_Letter)
                     Key_Index = Key_Index + 1  
 
+        x_Index = 0
+        Plaintext_Baudot_two = []
+        for x in Plaintext_Baudot:
+            if x != " ": 
+                Plaintext_Baudot_two.insert(x_Index, " ")
+            x_Index = x_Index + 1
         Plaintext_Baudot = Plaintext_Baudot.split(" ")
         st.write("".join(Plaintext_Baudot))
         st.write("The key is ", "".join(Key)) 
