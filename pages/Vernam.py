8745adc60e5284b289d4d2f755e95e3da49e8785
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-12 00:12:19 +0000

    Update Vernam.py

--- pages/Vernam.py
+++ pages/Vernam.py
@@ -231,9 +231,7 @@ with tab2:
                         Spaced_Encrypt_Key_Baudot.append(" | ")
                 st.write("".join(Spaced_Encrypt_Key_Baudot), " - Key")
                 st.write("".join(Ciphertext_Baudot), " - Ciphertext")
-                st.write( "The ciphertext is ", "".join(Ciphertext))   
-
-
+                st.write("The ciphertext is ", "".join(Ciphertext))   
 
 with tab3:
     st.header("Decrypt a ciphertext")
