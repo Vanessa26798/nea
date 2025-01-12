e7207f5305c245b019aed4e6a5edee58aec4a5ae
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-12 03:42:19 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,18 @@
 import streamlit as st
 import random
 
+col1, col2, col3 = st.columns(3)
+with col2:
+    st.title("ENCRYPTION TEACHING TOOL")
 
-st.title("ENCRYPTION TEACHING TOOL")
 st.write("Select the encryption method that you would like to discover more about")
 
 col1, col2 = st.columns(2)
-
 with col1:
     st.header("Caesar cipher")
     st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Caesar_cipher_left_shift_of_3.svg/1920px-Caesar_cipher_left_shift_of_3.svg.png")
     st.info('A substitution cipher where each letter of the plaintext is shifted ahead by a fixed number of letters (key).')
     st.page_link("pages/Caesar.py", label="Click me")
-
 with col2:
     st.header("Vernam cipher")
     st.image("https://www.cryptomuseum.com/crypto/svg/xor3.svg")
