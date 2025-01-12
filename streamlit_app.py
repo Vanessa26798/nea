f32b42717e4389364f3a4b2e35eeb43ed1fa7845
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-12 03:34:09 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,12 @@ col1, col2 = st.columns(2)
 with col1:
     st.header("Caesar cipher")
     st.info('A substitution cipher where each letter of the plaintext is shifted ahead by a fixed number of letters (key).')
+    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Caesar_cipher_left_shift_of_3.svg/1920px-Caesar_cipher_left_shift_of_3.svg.png")
     st.page_link("pages/Caesar.py", label="Click me")
 
 with col2:
     st.header("Vernam cipher")
     st.info('An encryption that uses one-time pad which contributes to its perfect security.')
+    st.image("https://www.cryptomuseum.com/crypto/svg/xor3.svg")
     st.page_link("pages/Vernam.py", label="Click me")
 
