a971374dc63d2c453c029a73e204b9c2046bbb2c
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-29 21:54:43 +0000

    Update Vernam.py

--- pages/Vernam.py
+++ pages/Vernam.py
@@ -143,6 +143,7 @@ with tab2:
         global Ciphertext_Baudot
         global Ciphertext
         global Correct_Encrypt_Key_Range
+        global Correct_Encrypt_Key_Length
         if Correct_Encrypt_Key_Range == True and Correct_Encrypt_Key_Length == True:
             if Encrypt_Choice == "1":
                 Encrypt_Key_Baudot = []
