ce28e0e22dec3a6aa728bebae34aed314528be46
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-29 09:29:33 +0000

    Update Vernam.py

--- pages/Vernam.py
+++ pages/Vernam.py
@@ -132,6 +132,7 @@ with tab2:
     
     def Check_Encrypt_Key_Length():
         global Correct_Encrypt_Key_Length
+        global Correct_Encrypt_Key_Range
         if Correct_Encrypt_Key_Range == True:
          if len(Encrypt_Key) != len(Plaintext) and Encrypt_Key_Input != "": 
              st.error('Key out of range.', icon="🚨")
