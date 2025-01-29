a73348085f5718c4dea4cd8ed1fcb81846c04dbb
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-29 21:12:48 +0000

    Update Vernam.py

--- pages/Vernam.py
+++ pages/Vernam.py
@@ -108,7 +108,7 @@ with tab2:
             Encrypt_Choice = st.text_input("Input your own key for encryption (1) or generate a random key (2)? ", value="")
 
     def Get_Encrypt_Key(): 
-        global Encrypt_Key_Input
+        global Encrypt_Key_Input        
         Encrypt_Key_Input = ""
         if Encrypt_Choice == "1":
             Encrypt_Key_Input = st.text_input("Please input the key for encryption, with the same length as the plaintext: ", value="")
@@ -118,6 +118,7 @@ with tab2:
 
     def Check_Encrypt_Key_Range():
         global Correct_Encrypt_Key_Range
+        Correct_Encrypt_Key_Range = False
         global Encrypt_Key
         Encrypt_Key_Index = 0
         Encrypt_Key = []
