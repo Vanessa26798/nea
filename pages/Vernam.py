72d1947a742904a9a57712a3d9095b742c9e9faa
Vanessa26798 <18clai@kendrick.reading.sch.uk>
2025-01-29 09:06:59 +0000

    Update Vernam.py

--- pages/Vernam.py
+++ pages/Vernam.py
@@ -121,7 +121,7 @@ with tab2:
 
 
     def Check_Encrypt_Key_Range():
-        global Correct_Encrypt_Key_Range
+        # global Correct_Encrypt_Key_Range
         global Encrypt_Key
         Encrypt_Key_Index = 0
         Encrypt_Key = []
@@ -142,7 +142,6 @@ with tab2:
              Correct_Encrypt_Key_Length = False
          elif len(Encrypt_Key) == len(Plaintext) and len(Encrypt_Key) != 0 and Encrypt_Key != "":
              Correct_Encrypt_Key_Length = True
-         # st.write(Correct_Encrypt_Key_Range)
 
     def Encryption():
         global Encrypt_Key_Baudot
